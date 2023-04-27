31ad7e4de9c5d90eb8211bb285f8d8eeb6d7e31a
nakajima-satoru <nakajima@email.com>
2023-04-28 06:57:31 +0900

    4/28

--- bin/Dialog.js
+++ bin/Dialog.js
@@ -1,9 +1,18 @@
-const $ = use("VDom");
+const Util = use("Util");
+const Dom = use("Dom");
 
 return class Dialog{
 
     open(contents){
 
+        var uid = Util.uniqId();
+        var baseHtml = use("DialogHtml");
+        baseHtml = Util.base64Decode(baseHtml);
+        baseHtml = baseHtml.split("{{uid}}").join(uid);
+        baseHtml = baseHtml.split("{{contents}}").join(contents);
+        Dom().insertAdjacentHTML("afterend", baseHtml);
+
+        return Dom("[uid=\"" + uid + "\"]");
     }
 
     loading(message){
